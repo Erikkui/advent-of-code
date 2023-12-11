f4094cc0fd055c1296d76fee82173d68c4585236
Erikkui <erik.kuitunen@outlook.com>
2023-12-11 14:06:34 +0200

    Added Day 11 part 2

--- 2023/Day_11/Day11_2.m
+++ 2023/Day_11/Day11_2.m
@@ -1,11 +1,13 @@
 clc
 close all
 clearvars
+format long
 
 rawdata = readlines( 'day11_data.txt' );
 rawdata = char( rawdata );
 
 [N, M] = size( rawdata );
+expandment = 1000000;
 
 data = zeros(N);
 duplicateRows = [];
@@ -23,38 +25,27 @@ for ii = 1:N
     for jj = 1:M
         elem = rawdata( ii, jj );
         if elem == '.'
-            data( ii, jj ) = 0;
-        elseif elem == '#'
             data( ii, jj ) = 1;
+        elseif elem == '#'
+            data( ii, jj ) = Inf;
         end
     end
 end
 
+%
 for ii = 1:length( duplicateRows )
     dupInd = duplicateRows(ii);
-
-    zeroes = zeros( 1, M );
-    upside = data( 1:dupInd, : );
-    downside = data( dupInd+1:end, : );
-
-    data = [ upside; zeroes; downside ];
-    duplicateRows = duplicateRows + 1;
-    N = N + 1;
+    data( dupInd, : ) = expandment*ones( 1, M );    
 end
 
 for ii = 1:length( duplicateCols )
     dupInd = duplicateCols(ii);
-
-    zeroes = zeros( N, 1 );
-    leftSide = data( :, 1:dupInd );
-    rightSide = data( :, dupInd+1:end );
-
-    data = [ leftSide, zeroes, rightSide ];
-    duplicateCols = duplicateCols + 1;
-    M = M + 1;
+    data( :, dupInd ) = expandment*ones( N, 1 );      
 end
 
-[galRows, galCols] =  find( data == 1 );
+
+%
+[galRows, galCols] =  find( data == Inf );
 pairNums = ( 1:length( galRows ) )';
 galInds = [ galRows, galCols, pairNums ];
 
@@ -63,14 +54,30 @@ pairs = [ galInds( pairs( :, 1 ), 1:2 ), galInds( pairs( :, 2 ), 1:2 ) ];
 
 steps = zeros( size( pairs, 1 ), 1 );
 for ii = 1:size( pairs, 1 )
-    rowStep = abs( pairs( ii, 1 ) - pairs( ii, 3) ); 
-    colStep = abs( pairs( ii, 2 ) - pairs( ii, 4) ); 
-    steps(ii) = rowStep + colStep;
+    pair1 = pairs( ii, 1:2 );
+    pair2 = pairs( ii, 3:4 );
+    rowMin = min( pair1(1), pair2(1) );
+    rowMax = max( pair1(1), pair2(1) );
+    colMin = min( pair1(2), pair2(2) );
+    colMax = max( pair1(2), pair2(2) );
+
+    rowStepsInds = rowMin:rowMax-1;
+    colStepsInds = colMin:colMax-1;
+
+    rowStep = data( rowMin, colStepsInds );
+    colStep = data( rowStepsInds, colMin );
+
+    totRowSteps = sum( rowStep( rowStep < Inf ) ) + sum( rowStep == Inf );
+    totColSteps = sum( colStep( colStep < Inf ) ) + sum( colStep == Inf );
+   
+    %pause
+    % clc
+    steps(ii) = totRowSteps + totColSteps;
 end
 
 sum(steps)
 
-
+% 702770569197
 
 
 
