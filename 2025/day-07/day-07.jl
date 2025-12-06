4399226803ec2abc1ae62a64c13d2e01907ccd60
Erikkui <erik.kuitunen@outlook.com>
2025-12-06 23:08:15 +0200

    Prepared next day's code file

---
+++ 2025/day-07/day-07.jl
@@ -0,0 +1,28 @@
+function read_txt(path)
+    open(path, "r") do f
+    rawdata = readlines(f)
+    return rawdata
+    end
+end
+
+function parse_input(rawdata)
+
+
+end
+
+
+function silver()
+    path = "2025/day-07/test_input.txt"
+    rawdata = read_txt( path )
+
+
+end
+
+
+
+function gold()
+    path = "2025/day-07/test_input.txt"
+    rawdata = read_txt( path )
+
+
+end
